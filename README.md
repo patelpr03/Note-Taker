5d8dfc76798b217b3d07dcfa7337cfa61838cf4a
priti patel <patelpr_03@yahoo.com>
2021-10-24 18:52:55 -0500

    full application

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Note-Taker
+
+
+## Table of Contents
+* [Description](#description)
+* [Technologies](#technologies)
+* [Preview](#preview)
+* [Deployed](#deployed)
+* [Questions](#questions)
+* [Credits](#Credits)
+
+
+##  Description
+For users that need to keep track of a lot of information, it's easy to forget and that is something very important. Note-taking is application for users that need to keep track of a lot of information. The application allows the user to take notes and have written information available when needed.
+
+## Technologies
+* JavaScript
+* HTML
+* CSS
+* Node.js
+* Express.js
+* UUID
+
+## Preview
+!["Screenshot"](public/assets/images/screenshot1.png)
+!["Screenshot"](public/assets/images/notetaker1.png)
+!["screenshot"](public/assets/images/screenshot.png)
+
+## Deployed
+
+## Questions
+Have questions about this project?  
+GitHub: https://github.com/patelpr03  
+Email:  patelpr_03@yahoo.com
+
+## Credits
+![GitHub license](https://img.shields.io/badge/Made%20by-%40Priti-orange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+http://localhost:3001/note
---
+++ server.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+//initializing express and naming it app
+const app = express();
+//PORT is the location where (locally) our app will be hosted
+const PORT = process.env.PORT || 3001; //not any number, for example 3306 is taken by SQL
+
+
+//Middleware, settings for your app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = req.body;
+   notes.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
+    res.json(notes);
+})
+
+// app.get('/api/test/:parameter', (req, res) => {
+//     res.json(req.params.parameter)
+// })
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const noteIndex = notes.findIndex(n => n.id == id);
+    notes.splice(noteIndex, 1);
+    fs.writeFileSync(
+     path.join(__dirname, './db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
